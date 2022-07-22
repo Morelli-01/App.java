eac32592aaeadfca895121ff69c3056730ec9121
Nicola Morelli <nicolamorelli30008@gmail.com>
2022-07-22 09:16:25 +0200

    22/07

--- app/src/main/java/MainProject/Graphics/LoginWindow.java
+++ app/src/main/java/MainProject/Graphics/LoginWindow.java
@@ -14,14 +14,24 @@ public class LoginWindow extends JFrame implements ActionListener {
 
     private static final JButton OKButton = new JButton("Ok");
     private static final JButton ExitButton = new JButton("Exit");
-    private static final JLabel  UserNameTF = new JLabel("User Name");
-    private static final JTextField CampoNome = new JTextField("UserName",20);
+    private static final JLabel UserNameTF = new JLabel("User Name");
+    private static final JTextField CampoNome = new JTextField("UserName", 20);
     private static final JLabel PSWTF = new JLabel("Password");
-    private static final JPasswordField CampoPSW = new JPasswordField("123456",20);
-    private static final JMenuItem helpItem =new JMenuItem("?");
+    private static final JPasswordField CampoPSW = new JPasswordField("123456", 20);
+    private static final JMenuItem helpItem = new JMenuItem("?");
     private static final JMenu questmenu = new JMenu("help");
-    private static final JMenuBar menuBar= new JMenuBar();
-    private int count =2;
+    private static final JMenuBar menuBar = new JMenuBar();
+    private int Tentativi = 3;
+    Boolean isDebugUser = false;
+
+    
+    public Boolean getIsDebugUser() {
+        return isDebugUser;
+    }
+
+    public void setIsDebugUser(Boolean isDebugUser) {
+        this.isDebugUser = isDebugUser;
+    }
 
     public String getUser() {
         return User;
@@ -39,8 +49,8 @@ public class LoginWindow extends JFrame implements ActionListener {
         Psw = psw;
     }
 
-    public void setGUI(GUI G){
-        this.G=G;
+    public void setGUI(GUI G) {
+        this.G = G;
     }
 
     public GUI getGUI() {
@@ -55,7 +65,7 @@ public class LoginWindow extends JFrame implements ActionListener {
         Credential = credential;
     }
 
-    public void setGUIVisible(boolean Value){
+    public void setGUIVisible(boolean Value) {
         G.setVisible(Value);
     }
 
@@ -81,25 +91,24 @@ public class LoginWindow extends JFrame implements ActionListener {
         panelButton.add(OKButton);
         panelButton.add(ExitButton);
 
-        JPanel mainPanel = new JPanel(new GridLayout(3,1));
+        JPanel mainPanel = new JPanel(new GridLayout(3, 1));
         mainPanel.add(panelName);
         mainPanel.add(panelPsw);
         mainPanel.add(panelButton);
 
-        setTitle("Login");    //nome della finestra
+        setTitle("Login"); // nome della finestra
         setJMenuBar(menuBar);
         setContentPane(mainPanel);
         setResizable(false);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setLocation(300,200); //posizione finestra(0 , 0 è l'angolo in alto dello schermo)
-        setSize(350,150);     // dimensioni finestra
-        setVisible(true);     // metodo per rendere la finestra visibile su schermo (con true)
+        setLocation(300, 200); // posizione finestra(0 , 0 è l'angolo in alto dello schermo)
+        setSize(350, 150); // dimensioni finestra
+        setVisible(true); // metodo per rendere la finestra visibile su schermo (con true)
     }
 
-    public static void exitSystem(){
+    public static void exitSystem() {
         System.exit(0);
     }
-
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==ExitButton){
@@ -108,14 +117,15 @@ public class LoginWindow extends JFrame implements ActionListener {
         if(e.getSource()==OKButton){
             String UserName = CampoNome.getText();
             String UserPsw = String.valueOf(CampoPSW.getPassword());
-            if (Credential.get(UserName)==null){
-                JOptionPane.showMessageDialog(null,"Credenziali errate, tentativi rimasti "+count+".","Error",
-                        JOptionPane.ERROR_MESSAGE);
-                return;
+            if (!Credential.containsKey(UserName)){
+                Tentativi--;
+                JOptionPane.showMessageDialog(null,"Credenziali errate, tentativi rimasti "+Tentativi+".","Error",
+                    JOptionPane.ERROR_MESSAGE);
             }
-            if(!Credential.get(UserName).equals(UserPsw)){
-             JOptionPane.showMessageDialog(null,"Credenziali errate, tentativi rimasti "+count+".","Error",
-                     JOptionPane.ERROR_MESSAGE);
+            else if(!Credential.get(UserName).equals(UserPsw)){
+                Tentativi--;
+                JOptionPane.showMessageDialog(null,"Credenziali errate, tentativi rimasti "+Tentativi+".","Error",
+                    JOptionPane.ERROR_MESSAGE);       
             }else{
 
              User=UserName;
@@ -123,11 +133,13 @@ public class LoginWindow extends JFrame implements ActionListener {
              System.out.println("Succesfull Login"); //Attivazione GUI di nick
              setGUIVisible(true);
              dispose();
+             if(User.equals("DebugUser")){
+                    setIsDebugUser(true);
+                }
             }
 
-            count--;
-            if(count<0){
-                JOptionPane.showMessageDialog(null,"numero di tentativi finiti, il programma verrà chiuso","Error",
+            if(Tentativi<=0){
+                JOptionPane.showMessageDialog(null,"Numero di tentativi finiti, il programma verrà chiuso","Error",
                         JOptionPane.ERROR_MESSAGE);
                 exitSystem();
             }
@@ -136,5 +148,5 @@ public class LoginWindow extends JFrame implements ActionListener {
             JOptionPane.showMessageDialog(null, "Inserire le credenziali per poter accedere all'applicazione", "Help", JOptionPane.INFORMATION_MESSAGE);
         }
     }
-
+    
 }
